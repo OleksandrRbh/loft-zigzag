56f819bdbf560e69fdd1b9f0b84555325a1d7824
OleksandrRbh <OleksandrRbh@gmail.com>
2019-11-20 08:06:01 +0200

    Fix close-btn in js file

--- source/js/script.js
+++ source/js/script.js
@@ -45,30 +45,21 @@ window.addEventListener("wheel", _.debounce((event) => {
 }, 150));
 
 
-
-
-
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navToggle.classList.remove('page-header__nav-toggle--closed');
+    navToggle.classList.add('page-header__nav-toggle--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navToggle.classList.add('page-header__nav-toggle--closed');
+    navToggle.classList.remove('page-header__nav-toggle--opened');
   }
 });
 
 mainNav.addEventListener('click', function() {  
   navMain.classList.remove('main-nav--opened');
   navMain.classList.add('main-nav--closed');
-});
-
-navToggle.addEventListener('click', function() {
-  if (navToggle.classList.contains('page-header__nav-toggle--closed')) {
-    navToggle.classList.remove('page-header__nav-toggle--closed');
-    navToggle.classList.add('page-header__nav-toggle--opened');
-  } else {
-    navToggle.classList.add('page-header__nav-toggle--closed');
-    navToggle.classList.remove('page-header__nav-toggle--opened');
-  }
 });
